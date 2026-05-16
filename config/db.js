195db136ca99847085f6aaa8d83ee9b0bb36a515
Furebo <furebodidace582@gmail.com>
2026-05-16 19:40:49 +0300

    updated cloudinary configuration file

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-       await mongoose.connect(process.env.MONGO_URI,{})
-       console.log("Connection to Mongo database established successfully.")
-    } catch (error) {
-        console.log("error connecting to mongodb: ",error)
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB connected:", conn.connection.host);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+};
+
 module.exports = connectDB;
